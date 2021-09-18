713973beb588cf8a8c8a0931c8a4a0f02a028068
juslenelobeu <juslenelobeu@gmail.com>
2021-09-18 12:35:22 -0300

    :construction: create and styled [Contact]

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,7 @@ function App() {
   return (
     <div className="container">
       <Header {...data.user} />
-      <Contact />
+      <Contact {...data.contact} />
       <Items />
     </div>
   );
--- src/components/Contact/index.tsx
+++ src/components/Contact/index.tsx
@@ -1,8 +1,26 @@
 import React from 'react'
+import './styles.scss'
+interface ContactProps {
+  whatsapp: string
+  phone: string
+  email: string
+}
 
-export default function Contact() {
+export default function Contact({ whatsapp, phone, email }: ContactProps) {
   return (
-    <div>
-    </div>
+    <section className="contact">
+      <a className="contact__link" href={whatsapp} target="_blank" rel="noopener noreferrer" title="Contato pelo WhatsApp">
+        <i className={`contact__icon fab fa-2x fa-whatsapp`}></i>
+        <span className="contact__name">WhatsApp</span>
+      </a>
+      <a className="contact__link" href={phone} target="_blank" rel="noopener noreferrer" title="Contato pelo WhatsApp">
+        <i className={`contact__icon fas fa-2x fa-phone-alt`}></i>
+        <span className="contact__name">Phone</span>
+      </a>
+      <a className="contact__link" href={email} target="_blank" rel="noopener noreferrer" title="Contato pelo WhatsApp">
+        <i className={`contact__icon far fa-2x fa-envelope`}></i>
+        <span className="contact__name">E-mail</span>
+      </a>
+    </section>
   )
 }
